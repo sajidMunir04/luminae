d2f1cf1a56d21021975027e127280cb4aadf0b49
SAJID <sajid.munir4428@hotmail.com>
2024-06-01 11:20:33 +0500

    removed invalid pages

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,6 +1,5 @@
 import '../src/app/globals.css';
 import HeaderTemplate from '../src/app/shared/HeaderTemplate';
-import StoreInteractionContainer from "../src/app/shared/ProductCategoriesManager";
 import FooterTemplate from '../src/app/shared/FooterTemplate';
 import ProductsCart from '../src/app/cart/Cart';
 
