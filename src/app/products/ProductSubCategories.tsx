97cf3a3aeebb0c51af60c4c5639763e73897b41d
SAJID <sajid.munir4428@hotmail.com>
2024-06-01 13:13:21 +0500

    Update ProductSubCategories.tsx

--- src/app/products/ProductSubCategories.tsx
+++ src/app/products/ProductSubCategories.tsx
@@ -24,8 +24,8 @@ function ProductSubCatergories(props : Props)
     return (<div onMouseEnter={() => setHoverState(true)} onTouchStart={() => setHoverState(true)} className={styles.container}><motion.p className={styles.categoryButton} whileHover={{scale: 1.5}}>{props.mainCategory}</motion.p>
         {isHovered && <motion.div  initial={{scaleX: 0}} whileInView={{scaleX: 1}} onMouseEnter={() => setHoverState(true)} onTouchStart={() => setHoverState(true)} onMouseLeave={() => setHoverState(false)} onTouchCancel={() => setHoverState(false)} className={styles.subCategoriescontainer}>
             {props.categories.map((item) => (
-                <motion.a className={styles.categoryLink} whileHover={{scale: 1.05}} onClick={()=>{handleCategoryClick('http://localhost:3000/' + props.mainCategory + '/' + item)}} 
-                key={item} href={'http://localhost:3000/' + props.mainCategory + '/' + item}>{item}</motion.a>
+                <motion.a className={styles.categoryLink} whileHover={{scale: 1.05}} onClick={()=>{handleCategoryClick(process.env.BASE_URL + props.mainCategory + '/' + item)}} 
+                key={item} href={process.env.BASE_URL + props.mainCategory + '/' + item}>{item}</motion.a>
             ))}
         </motion.div>}
     </div>);
